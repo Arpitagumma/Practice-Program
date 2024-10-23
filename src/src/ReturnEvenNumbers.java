04cf60124c76667adf54449ddc28bb501ed04c6f
Arpitagumma <gummaarpita@gmail.com>
2024-10-23 19:08:59 +0530

    Hello world program

---
+++ src/src/ReturnEvenNumbers.java
@@ -0,0 +1,16 @@
+public class ReturnEvenNumbers {
+    public static void main(String[] args) {
+        int[] numbers={12,4,2,1,53,64,14,54,33};
+        System.out.println("even numbers in array");
+        for(int num:numbers){
+
+
+       if(num%2==0){
+           System.out.println(num+"");
+        }
+       }
+
+
+    }
+
+}
